af919c066edb9f7ec37c9a19f7d03f072c620111
zhouchen <you@example.com1583741285@qq.com  git config --global user.name Your>
2018-12-04 17:10:10 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,18 @@
 <template>
-	<div>hello world!</div>
+	<div>
+		<home-header></home-header>
+	</div>
 	
 </template>
 
 
 <script>
+import HomeHeader from './components/Header.vue'
 export default{
-	name:'Home'
+	name:'Home',
+	components:{
+		HomeHeader
+	}
 }	
 
 </script>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+	<div class="header">
+		 <div class="header-left">
+          <div class="iconfont back-icon">&#xe624;</div>
+		 </div>
+		 <div class="header-input">
+		 	 <span class="iconfont input-icon">&#xe632;</span>
+		 	 输入城市/景点/游玩主题
+		 </div>
+		 <div class="header-right">城市
+          <span class="iconfont arraw-icon">&#xe6aa;</span>
+		 </div>
+	</div>
+	
+</template>
+
+
+<script>
+export default{
+	name:'Header'
+}	
+
+</script>
+
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+		.header {
+			display:flex
+			line-height:.86rem
+			background:$bgColor
+			color:#fff
+		}
+		.header-left{
+		  	width: .64rem
+            float: left
+		  }
+		  .back-icon{
+		  	text-align: center
+			font-size: .4rem
+		  }
+		  .header-input{
+		  	flex: 1
+			// width:100%
+			height:.64rem
+			line-height:.64rem
+			margin-top:.12rem
+			margin-left:.2rem
+			background:#fff
+			border-radius:.1rem
+			color:#ccc
+		  	padding-left:.2rem
+		  } 
+		  .input-icon{
+		  	font-size:.26rem
+		  	padding-right:.06rem
+		  }
+		  .header-right{
+		  	width: 1.24rem
+			float: right
+			text-align: center
+		  }
+		  .arraw-icon{
+		  	margin-left:-0.04rem
+		  	font-size: .24rem
+		  }
+		 
+		 	 		
+		 	 		
+		 	 		
+		 
+          
+</style>
